6db51c0393d26d301a0b934f9fabb1befbf57f35
Grazielle <164579007+grazielle2012@users.noreply.github.com>
2024-05-11 20:29:53 -0300

    pagina historico - mudanças

--- codigo-fonte/diario.html/scriptDiario.js
+++ codigo-fonte/diario.html/scriptDiario.js
@@ -3,3 +3,8 @@ elem.addEventListener('click', function () {
 alert("Enviado");
 }, false);
 
+function enviar() {
+    var texto = document.getElementByclass('submit').value;
+    alert('Texto digitado: ' + texto);
+    // Aqui você pode enviar o texto para onde quiser
+}
