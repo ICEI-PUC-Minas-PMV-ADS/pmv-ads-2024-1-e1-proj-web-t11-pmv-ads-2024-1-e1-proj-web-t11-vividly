103cf1b1e4bded6f44665b477b5003b2f4ccabb0
Grazielle <164579007+grazielle2012@users.noreply.github.com>
2024-05-10 22:12:34 -0300

    Diario
    Pequenas mudanças

---
+++ codigo-fonte/diario.html/scriptDiario.js
@@ -0,0 +1,7 @@
+<script>
+function enviar() {
+    var texto = document.getElementByclass('submit').value;
+    alert('Texto digitado: ' + texto);
+    // Aqui você pode enviar o texto para onde quiser
+}
+</script>
