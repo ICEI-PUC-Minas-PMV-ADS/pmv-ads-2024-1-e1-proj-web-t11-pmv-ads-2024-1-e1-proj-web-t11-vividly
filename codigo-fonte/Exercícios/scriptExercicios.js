3cbec9b407fc8246b2af0985c9d072615a4fa74a
Eduardo Soares <eduardosoares-1@hotmail.com>
2024-06-08 23:51:15 -0300

    Salvar_Comentarios_localStorage

--- codigo-fonte/Exercícios/scriptExercicios.js
+++ codigo-fonte/Exercícios/scriptExercicios.js
@@ -1,7 +1,10 @@
 const userName = JSON.parse(localStorage.getItem("usuarios"));
 const usuario = userName[0].usuario;
 
+//Exibir localStorage
+
 // Vídeo 1
+// Criar comentário
 
 const textComment = document.getElementById('comentario');
 const form = document.getElementById('formulario');
@@ -19,6 +22,8 @@ form.addEventListener('submit', (event) =>{
     textComment.focus();
 });
 
+// Mostrar comentários
+
 var ativarComentario = document.getElementById('container-comentarios');
 var display = 1;
 
@@ -31,10 +36,41 @@ function ativarComentarios() {
         ativarComentario.style.display = 'none';
         display = 1;
     }
-
 }; 
 
+// Salvar comentario no localStorage 
+
+var comentarioSalvo = [];
+
+function addComment() {
+    if(localStorage.comentarios){
+        comentarioSalvo = JSON.parse(localStorage.getItem('comentarios'));
+    }
+
+    let novoComentario = textComment.value;
+    comentarioSalvo.push({usuario, novoComentario});
+    localStorage.comentarios = JSON.stringify(comentarioSalvo);
+}
+
+// Exibir comentário salvo
+
+window.onload = function showComment(){
+    
+    if(localStorage.comentarios){
+        comentarioSalvo = JSON.parse(localStorage.getItem('comentarios'));
+    } 
+
+    for(var i in comentarioSalvo){
+        let p = document.createElement('p');
+        p.classList = 'comentario-exibido';
+        p.innerHTML = `<strong>${comentarioSalvo[i].usuario}: </strong>${comentarioSalvo[i].novoComentario}`;
+        commentPost.append(p);
+    }
+}
+
+
 //Video2
+// Criar comentário
 
 const textComment2 = document.getElementById('comentario2');
 const form2 = document.getElementById('formulario2');
@@ -52,6 +88,7 @@ form2.addEventListener('submit', (event) =>{
     textComment2.focus();
 });
 
+// Mostrar comentários
 
 var ativarComentario2 = document.getElementById('container-comentarios2');
 var display2 = 1;
@@ -69,6 +106,7 @@ function ativarComentarios2() {
 }; 
 
 //Video3
+// Criar comentário
 
 const textComment3 = document.getElementById('comentario3');
 const form3 = document.getElementById('formulario3');
@@ -86,6 +124,8 @@ form3.addEventListener('submit', (event) =>{
     textComment3.focus();
 });
 
+// Mostrar comentários
+
 var ativarComentario3 = document.getElementById('container-comentarios3');
 var display3 = 1;
 
@@ -101,6 +141,7 @@ function ativarComentarios3() {
 }; 
 
 //Video4
+// Criar comentário
 
 const textComment4 = document.getElementById('comentario4');
 const form4 = document.getElementById('formulario4');
@@ -118,6 +159,8 @@ form4.addEventListener('submit', (event) =>{
     textComment4.focus();
 });
 
+// Mostrar comentários
+
 var ativarComentario4 = document.getElementById('container-comentarios4');
 var display4 = 1;
 
@@ -133,6 +176,7 @@ function ativarComentarios4() {
 }; 
 
 //Video5
+// Criar comentário
 
 const textComment5 = document.getElementById('comentario5');
 const form5 = document.getElementById('formulario5');
@@ -149,6 +193,8 @@ form5.addEventListener('submit', (event) =>{
     textComment5.value = '';
     textComment5.focus();
 });
+
+// Mostrar comentários
 
 var ativarComentario5 = document.getElementById('container-comentarios5');
 var display5 = 1;
